c2ac15ca52b9a131e69166716bd663f7dc1c1200
DivineMatongo <dstanley3247@gmail.com>
2026-03-13 16:41:44 +0200

    Add function to get choice from user

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 function getComputerChoice() {
-    choice = Math.floor((Math.random() * 3));
-    choiceWords = ["Rock", "Paper", "Scissors"];
+    let choice = Math.floor((Math.random() * 3));
+    let choiceWords = ["Rock", "Paper", "Scissors"];
     return choiceWords[choice];
+}
+
+function getHumanChoice() {
+    let message = "Rock, paper, or scissors?";
+    let choice;
+    while (true) {
+        choice = prompt(message).toUpperCase().trim();
+        if ((choice === "ROCK") || (choice === "R")) {
+            return "Rock";
+        } else if ((choice === "PAPER") || (choice === "P")) {
+            return "Paper";
+        } else if ((choice === "SCISSORS") || (choice === "S")) {
+            return "Scissors";
+        } else {
+            message = "Invalid input. Please try again.\n" +
+                       "Rock, paper, or scissors?";
+        }
+    }
 }
