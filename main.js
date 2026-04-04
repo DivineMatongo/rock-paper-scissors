83860339b6eaa6eeb2641a43731d80f311051901
DivineMatongo <dstanley3247@gmail.com>
2026-04-05 01:04:51 +0200

    Migrate Rock Paper Scissors code to GUI version
    - Scores are now tracked by global variables
    - playGame() function removed. The game now runs continuously through
    event listeners until a score of 5 is achieved by either the user or
    the computer.

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const buttons = document.querySelector("#rps-buttons");
+const playButton = document.querySelector("#play-quit");
+const gameScreen = document.querySelector("#screen");
+
+const TITLE = "Rock Paper Scissors";
+const TARGET_MESSAGE = "First one to 5 wins!";
+const START_MESSAGE = "Click Start to play.";
+const PROMPT = "Rock Paper Scissors?";
+
+const line1 = document.querySelector("#line1");
+const line2 = document.querySelector("#line2");
+const line3 = document.querySelector("#line3");
+
 // Function used by computer to randomly select rock, paper, or scissors
 function getComputerChoice() {
     let choice = Math.floor((Math.random() * 3));
@@ -24,8 +40,8 @@ function getHumanChoice() {
     }
 }
 
-// Returns: String "x" or "y", depending on whether the first or second
-// argument wins, respectively, based on the rules of rock, paper, scissors.
+// Returns: String "x" or "y" or "draw", depending on whether the first or second
+// argument wins, or it's a draw, based on the rules of rock, paper, scissors.
 // Expects: two string arguments, each either "rock", "paper", or "scissors"
 function xVersusY(x, y) {
     try {
@@ -66,37 +82,61 @@ function xVersusY(x, y) {
                         `'${x}' and '${y}'`);
 }
 
-// Plays five rounds with user, then displays winner 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    // Evaluates one round and displays the result on the console
-    function playRound(humanChoice, computerChoice) {
-        winner = xVersusY(humanChoice, computerChoice);
-        if (winner === "x"){
-            humanScore += 1;
-            console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-        } else if (winner === "y") {
-            computerScore += 1;
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-        } else {
-            console.log(`Draw. You both chose ${computerChoice}.`);
-        }
+function startStopGame(start) {
+    for (const btn of buttons.children) {
+        btn.disabled = !start;
     }
-
-    for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    console.log("--- GAME OVER ---");
-    if (humanScore > computerScore)
-        console.log("You win!");
-    else if (computerScore > humanScore)
-        console.log("You lose!");
-    else
-        console.log("It's a draw!");
-
-    console.log(`You ${humanScore} - ${computerScore} Computer`);
+    playButton.textContent = start ? "Quit" : "Start Game";
 }
 
-// playGame();
+buttons.addEventListener("click", (e) => {
+    let computerChoice = getComputerChoice();
+    let humanChoice = e.target.textContent;
+
+    winner = xVersusY(humanChoice, computerChoice);
+    if (winner === "x") {
+        humanScore += 1;
+    } else if (winner === "y") {
+        computerScore += 1;
+    }
+
+    if (humanScore >= 5 || computerScore >= 5) {
+        if (winner === "x") {
+            line1.textContent = `Great! ${humanChoice} beats ${computerChoice}`;
+            line2.textContent = `You win ${humanScore} - ${computerScore}!`;
+        } else if (winner === "y") {
+            line1.textContent = `Sorry. ${computerChoice} beats ${humanChoice}`;
+            line2.textContent = `You lost ${humanScore} - ${computerScore}`;
+        }
+        line3.textContent = "Play again?";
+        startStopGame(false);
+        humanScore = computerScore = 0;
+
+    } else {
+        line1.textContent = `You ${humanScore} - ${computerScore} Computer`;
+        if (winner === "x") {
+            line2.textContent = `Great! ${humanChoice} beats ${computerChoice}`;
+        } else if (winner === "y") {
+            line2.textContent = `Sorry. ${computerChoice} beats ${humanChoice}`;
+        } else {
+            line2.textContent = `You both chose ${humanChoice}`;
+        }
+        line3.textContent = "";
+    }
+});
+
+playButton.addEventListener("click", () => {
+    console.log(playButton.textContent);
+    if (playButton.textContent === "Start Game") {
+        startStopGame(true);
+        line1.textContent = `You ${humanScore} - ${computerScore} Computer`;
+        line2.textContent = PROMPT;
+        line3.textContent = "";
+    } else {
+        startStopGame(false);
+        line1.textContent = TITLE;
+        line2.textContent = TARGET_MESSAGE;
+        line3.textContent = START_MESSAGE;
+        humanScore = computerScore = 0;
+    }
+});
